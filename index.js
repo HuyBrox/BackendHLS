61faa6aa066a195e6f1a9125a65a3cd0d4595779
HuyBrox <128505633+HuyBrox@users.noreply.github.com>
2024-12-21 17:05:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors(corsOptions));
 
 // Middleware cho headers CORS tùy chỉnh
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://https://hls-j3u6.onrender.com');
+    res.header('Access-Control-Allow-Origin', 'https://hls-sand.vercel.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -33,9 +33,9 @@ app.use((req, res, next) => {
 
 // Xử lý preflight request
 app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://https://hls-j3u6.onrender.com');
+    res.header('Access-Control-Allow-Origin', 'https://hls-sand.vercel.app');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.send();
 });
