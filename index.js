e8e4480ccb1e1bd94d79eb133dc08fe843363c80
HuyBrox <128505633+HuyBrox@users.noreply.github.com>
2024-12-21 15:45:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,23 @@ const corsOptions = {
     credentials: true,
 };
 app.use(cors(corsOptions));
+// Middleware cho headers CORS tùy chỉnh
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://hls-sand.vercel.app');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
+// Xử lý preflight request
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'https://hls-sand.vercel.app');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.send();
+});
 // Tích hợp PeerServer vào Express
 const peerServer = ExpressPeerServer(server, {
     debug: true,       // Hiển thị thông tin debug
